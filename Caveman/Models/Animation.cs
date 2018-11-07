9691d108bf87a536bb5d316df6937e4b3add80e6
Ezequiel Aciar <brianaciarezequiel@gmail.com>
2018-11-07 01:14:13 -0300

    Full moves for Caveman + No interruptible animations

--- Caveman/Models/Animation.cs
+++ Caveman/Models/Animation.cs
@@ -9,27 +9,27 @@ namespace Caveman.Models
 {
     public class Animation
     {
-        public static bool NotLoopeable = false;
-        public static bool Loopeable = true;
+        public static bool NotInterruptible = false;
+        public static bool Interruptible = true;
 
         public int CurrentFrame { get; set; }
         public int FrameCount { get; private set; }
         public int FrameHeight { get { return Texture.Height; } }
         public float FrameSpeed { get; set; }
         public int FrameWidth { get { return Texture.Width / FrameCount; } }
+        public bool IsInterruptible { get; set; }
         public bool IsLooping { get; set; }
-        public bool IsLoopeable { get; set; }
 
         public Texture2D Texture { get; private set; }
 
-        public Animation(Texture2D texture, int frameCount, float frameSpeed, bool isLoopeable)
+        public Animation(Texture2D texture, int frameCount, float frameSpeed, bool isInterruptible)
         {
             this.Texture = texture;
             this.FrameCount = frameCount;
-            this.IsLooping = true;
             this.FrameSpeed = frameSpeed;
+            this.IsLooping = false;
             this.CurrentFrame = 0;
-            this.IsLoopeable = isLoopeable;
+            this.IsInterruptible = isInterruptible;
         }
     }
 }
--- Caveman/Models/Caveman.cs
+++ Caveman/Models/Caveman.cs
@@ -13,7 +13,7 @@ namespace Caveman.Models
     public class Caveman : Sprite
     {
         float _timer;
-        private float VelocityX;
+        private float VelocityX = 2;
         private float VelocityY =0.0f;
         private float gravity = 0.5f;
         private bool Jumping = false;
@@ -26,61 +26,41 @@ namespace Caveman.Models
 
         public override void LoadContent(ContentManager content)
         {
-            Dictionary<String, Animation> animDict = new Dictionary<string, Animation> {
+            this.AnimationsDict = new Dictionary<string, Animation> {
                 {"Standing",
-                    new Animation(content.Load<Texture2D>("CavemanStanding_sm"),4,0.4f,Animation.Loopeable)
+                    new Animation(content.Load<Texture2D>("CavemanStanding_sm"),4,0.4f,Animation.Interruptible)
                 },
-                {"Running",
-                    new Animation(content.Load<Texture2D>("CavemanRunning_sm"),6,0.2f,Animation.Loopeable)
+                {"Running_Right",
+                    new Animation(content.Load<Texture2D>("CavemanRunning_sm"),6,0.2f,Animation.Interruptible)
                 },
                 {"Running_left",
-                    new Animation(content.Load<Texture2D>("CavemanRunningLeft_sm"),6,0.2f,Animation.Loopeable)
+                    new Animation(content.Load<Texture2D>("CavemanRunningLeft_sm"),6,0.2f,Animation.Interruptible)
                 },
                 {"Jumping",
-                    new Animation(content.Load<Texture2D>("CavemanJumping_sm"),7,0.3f,Animation.NotLoopeable)
+                    new Animation(content.Load<Texture2D>("CavemanJumping_sm"),7,0.2f,Animation.NotInterruptible)
                 },
                 {"Hitting",
-                    new Animation(content.Load<Texture2D>("CavemanHitting_sm"),5,0.1f,Animation.NotLoopeable)
+                    new Animation(content.Load<Texture2D>("CavemanHitting_sm"),5,0.1f,Animation.NotInterruptible)
+                },
+                {"Throwing",
+                    new Animation(content.Load<Texture2D>("CavemanThrowing"),3,0.2f,Animation.NotInterruptible)
                 },
             };
 
-            this.AnimationsDict = animDict;
+            this.currentAnimation = AnimationsDict["Standing"];
         }
 
         public override void Update(ref GameTime gameTime)
         {
-            currentAnimation = AnimationsDict["Standing"];
-            currentAnimation.IsLooping = false;
-
-            #region Keys
-            if (Keyboard.GetState().IsKeyDown(Keys.C))
+            ExecuteMoves();
+            if (currentAnimation.IsInterruptible || !currentAnimation.IsLooping)
             {
-                currentAnimation = AnimationsDict["Hitting"];
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
-            {
-                if (!currentAnimation.IsLooping)
-                {
-                    this.Position = new Vector2(this.Position.X + 2, this.Position.Y);
-                    currentAnimation = AnimationsDict["Running"];
-                }
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
-            {
-                this.Position = new Vector2(this.Position.X - 2, this.Position.Y);
-                currentAnimation = AnimationsDict["Running_left"];
+                currentAnimation = AnimationsDict["Standing"];
+                currentAnimation.IsLooping = true;
+                SetAnimation();
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Space))
-            {
-                currentAnimation = AnimationsDict["Jumping"];
-                StartJumping();
-            }
-            if (Keyboard.GetState().IsKeyUp(Keys.Space)) {
-                EndJumping();
-            }
-            #endregion
-
+            #region Gravity and Y Coordinates
             VelocityY += gravity;
             this.Position += new Vector2(0, VelocityY);
 
@@ -90,7 +70,9 @@ namespace Caveman.Models
                 VelocityY = 0.0f;
                 Jumping = false;
             }
+            #endregion
 
+            #region Manage Animation Frames
             _timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             if (_timer > currentAnimation.FrameSpeed)
@@ -104,6 +86,57 @@ namespace Caveman.Models
                     currentAnimation.CurrentFrame = 0;
                     currentAnimation.IsLooping = false;
                 }
+            }
+            #endregion
+        }
+        private void ExecuteMoves()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                MoveForward();
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                MoveBackwards();
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                StartJumping();
+            }
+
+            if (Keyboard.GetState().IsKeyUp(Keys.Space))
+            {
+                EndJumping();
+            }
+        }
+        private void SetAnimation()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.C))
+            {
+                currentAnimation = AnimationsDict["Hitting"];
+                currentAnimation.IsLooping = true;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.X)) {
+                currentAnimation = AnimationsDict["Throwing"];
+                currentAnimation.IsLooping = true;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                currentAnimation = AnimationsDict["Running_Right"];
+                currentAnimation.IsLooping = true;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                currentAnimation = AnimationsDict["Running_left"];
+                currentAnimation.IsLooping = true;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                currentAnimation = AnimationsDict["Jumping"];
+                currentAnimation.IsLooping = true;
             }
         }
 
@@ -127,7 +160,16 @@ namespace Caveman.Models
 
         internal void MoveBackwards()
         {
-            this.Position = new Vector2(this.Position.X -2, this.Position.Y);
+            this.Position = new Vector2(this.Position.X - VelocityX, this.Position.Y);
+            if(this.Position.X <0)
+                this.Position = new Vector2(0, this.Position.Y);
+        }
+
+        internal void MoveForward()
+        {
+            this.Position = new Vector2(this.Position.X + VelocityX, this.Position.Y);
+            if (this.Position.X > Game1.Bounds.Width)
+                this.Position = new Vector2(Game1.Bounds.Width, this.Position.Y);
         }
 
         public override void Draw(ref SpriteBatch spriteBatch, ref GameTime gameTime)
